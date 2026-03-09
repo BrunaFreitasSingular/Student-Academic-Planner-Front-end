6a0c66ab79f11ac8c6be3a6c89c515ca59ef00a8
Bruna <bruna.freitas@singularapps.co>
2026-03-09 20:14:14 -0300

    feat(navbar): add login page link

--- my-app/components/Navbar.tsx
+++ my-app/components/Navbar.tsx
@@ -5,6 +5,7 @@ export function Navbar() {
     <nav className="flex gap-6 bg-gray-200 p-4">
       <Link href="/" className="font-medium hover:text-blue-600">Home</Link>
       <Link href="/subjects" className="font-medium hover:text-blue-600">Subjects</Link>
+      <Link href="/login" className="font-medium hover:text-blue-600">Login</Link>
     </nav>
   );
 }
