e1e3c4f335b84410d9df8a8553fe399aeb398d6d
Bruna <bruna.freitas@singularapps.co>
2026-03-09 15:38:50 -0300

    feat: create layout

---
+++ my-app/app/layout.tsx
@@ -0,0 +1,19 @@
+import "./globals.css";
+import { Navbar } from "../components/Navbar";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="pt-br">
+      <body className="bg-gray-100">
+        <Navbar />
+        <main className="p-8">
+          {children}
+        </main>
+      </body>
+    </html>
+  );
+}
