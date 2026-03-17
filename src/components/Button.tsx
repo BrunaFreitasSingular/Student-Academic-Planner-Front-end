732772933838c7ad2a535a646aea76df69718300
Bruna <bruna.freitas@singularapps.co>
2026-03-17 10:12:12 -0300

    refactor: variants of the button component have been changed

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,13 +1,13 @@
 import { ButtonHTMLAttributes } from "react"
 
 type ButtonProps = {
-  variant?: "common" | "small" | "medium" | "large"
+  variant?: "primary" | "secondary" | "textButton"
   href?: string
   children: React.ReactNode
 } & ButtonHTMLAttributes<HTMLButtonElement>
 
 export function Button({
-  variant = "common",
+  variant = "primary",
   children,
   ...props
 }: ButtonProps) {
@@ -16,10 +16,9 @@ export function Button({
     "px-4 py-2 font-medium inline-block rounded-lg flex items-center justify-center"
 
   const variants = {
-    common: "bg-gray-400 text-white hover:bg-gray-600 transition",
-    small: "text-xs bg-gray-400 text-white hover:bg-gray-600 transition",
-    medium: "text-sm bg-gray-400 text-white hover:bg-gray-600 transition",
-    large: "text-lg px-3 py-1 bg-gray-400 text-white hover:bg-gray-600 transition"
+    primary: "text-sm bg-gray-400 text-white hover:bg-gray-600 transition", 
+    secondary: "text-xs bg-gray-400 text-white hover:bg-gray-600 transition",
+    textButton: "text-xs bg-gray-400 text-white hover:bg-gray-600 transition"
   }
 
   const className = `${base} ${variants[variant]}`
