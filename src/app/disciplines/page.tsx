d854fe540dd5eb5f6ddaef10bc512be4553cfc60
Bruna <bruna.freitas@singularapps.co>
2026-03-18 17:37:01 -0300

    refactor: clear disciplines page to prepare for new history layout

--- src/app/disciplines/page.tsx
+++ src/app/disciplines/page.tsx
@@ -1,33 +1,11 @@
-"use client"
-import { useState } from "react";
-import { addDiscipline } from "../actions/disciplines";
-import { Button } from "@/src/components/Button";
-import { DisciplineForm } from "@/src/components/DisciplineForm"
-import { LinkComponet } from "@/src/components/Link";
+//"use client"
 
 export default function SubjectsPage() {
-
-  const [showForm,setShowForm] = useState(false);
-
-  const openForm = () => {setShowForm(true)}
-  const closeForm = () => {setShowForm(false)}
-
   return (
     <div>
-      <Button variant="secondary" onClick={openForm}>
-        Adicionar Disciplina
-      </Button>
-
-      <LinkComponet href="/" variant="primary">
-        Voltar para Home
-      </LinkComponet>
-      
-      {showForm && 
-        <DisciplineForm
-          action={addDiscipline}
-          onClose={closeForm}
-        />}
+      <main>
     
+      </main>
     </div>
   );
 }
